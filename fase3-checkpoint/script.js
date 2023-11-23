c200ea92cfb2a9d3e311d6bef3e6421d33098b55
bdisfer1410 <145486867+bdisfer1410@users.noreply.github.com>
2023-11-23 13:45:48 +0100

    Subida de cosas

---
+++ fase3-checkpoint/script.js
@@ -0,0 +1,32 @@
+// Selectores de elementos del HTML
+const nav = document.querySelector("nav");
+
+const dropdown = document.querySelector("#dropdown");
+const button = dropdown.querySelector("button");
+const list = dropdown.querySelector("ul");
+
+
+// Funcionalidad para mostrar u ocultar el desplegable
+let dropdown_visible = false;
+
+function set_dropdown(visible) {
+    list.style.display = visible ? "block" : "none";
+    dropdown_visible = visible;
+}
+
+function show_dropdown() {
+    set_dropdown(true);
+}
+
+function hide_dropdown() {
+    set_dropdown(false);
+}
+
+function toggle_dropdown() {
+    set_dropdown(!dropdown_visible);
+}
+
+// Implementación de dicha funcionalidad
+dropdown.addEventListener("click", toggle_dropdown);
+button.addEventListener("mouseover", show_dropdown);
+nav.addEventListener("mouseleave", hide_dropdown);
